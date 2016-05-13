1afe1ceabce9d933e223b7ba48e4df33893385b6
Jannik <polymehr.service@gmail.com>
2016-05-13 22:23:40 +0200

    Fix indent and line endings
    * Fix Format of some files to be the same as the other files
      * Fix 2 space indent in some old Eclipse files
      * Fix DOS line endings in some files
    
      * Ignore files staged for removal in the future

--- src/polyplot/graphics/PlotterController.java
+++ src/polyplot/graphics/PlotterController.java
@@ -1,47 +1,46 @@
 package polyplot.graphics;
 
 
+import javax.swing.*;
 import java.awt.event.WindowAdapter;
 import java.awt.event.WindowEvent;
 
-import javax.swing.*;
-
 public class PlotterController extends JFrame {
 
-  private static final long serialVersionUID = 6483735967730423588L;
-  
-  private FunctionPlotter plotter;
-  
-  public PlotterController() {
-    super("PolyPlot");
-    int px = 600;
-    
-    this.setSize(px,px);
+    private static final long serialVersionUID = 6483735967730423588L;
 
-    this.add(plotter = new FunctionPlotter(20));
+    private FunctionPlotter plotter;
 
-    this.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+    public PlotterController() {
+        super("PolyPlot");
+        int px = 600;
 
-    this.setLocationRelativeTo(null);
+        this.setSize(px, px);
 
-    this.setVisible(true);
-    
-    
-    this.addWindowListener(new WindowAdapter() {
-      @Override
-      public void windowIconified(WindowEvent e) {
-        double rand = Math.random(); // A little easter egg...
-        setTitle(rand <= 0.001 ? "Ame should really visit his Mom and not this window!" 
-            : rand <= 0.1 ? "Dean's other other function plotter" : "PolyPlot");
-      }
-    });
-  }
-  
-  /**
-   * Returns the {@link FunctionPlotter} of this <code>PlotterController</code>.
-   */
-  public FunctionPlotter getFunctionPlotter() {
-    return plotter;
-  }
+        this.add(plotter = new FunctionPlotter(20));
+
+        this.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+
+        this.setLocationRelativeTo(null);
+
+        this.setVisible(true);
+
+
+        this.addWindowListener(new WindowAdapter() {
+            @Override
+            public void windowIconified(WindowEvent e) {
+                double rand = Math.random(); // A little easter egg...
+                setTitle(rand <= 0.001 ? "Ame should really visit his Mom and not this window!"
+                        : rand <= 0.1 ? "Dean's other other function plotter" : "PolyPlot");
+            }
+        });
+    }
+
+    /**
+     * Returns the {@link FunctionPlotter} of this <code>PlotterController</code>.
+     */
+    public FunctionPlotter getFunctionPlotter() {
+        return plotter;
+    }
 
 }
