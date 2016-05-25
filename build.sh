ef1b1971f8b2d4346f8146c0b48309c356a0a3b7
5hir0kur0 <12101162+5hir0kur0@users.noreply.github.com>
2016-05-25 16:19:23 +0200

    Improve build script
    * Only pass files to the compiler (in theory there could be a directory
      named "X.java" and that would have been passed to the compiler in the
      old version)
    * Clean up after the compiling is done

--- build.sh
+++ build.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 
 [ -d tmp ] || mkdir tmp
-find src -name "*.java" -print0 | xargs -0 javac -d tmp
+find src -type f -name '*.java' -print0 | xargs -0 javac -d tmp
 echo 'Main-Class: polyplot.PolyPlot' > tmp/manifest.txt
 jar cfm polyplot.jar tmp/manifest.txt -C tmp .
+rm -r tmp
